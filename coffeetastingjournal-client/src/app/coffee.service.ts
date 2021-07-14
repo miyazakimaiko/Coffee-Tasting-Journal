a9a1b3f9cf0b16ddacfab4bb9abb09ae3b107ea6
miyazakimaiko <myzkmik19922@gmail.com>
2021-07-14 23:26:01 +0100

    Created service

---
+++ coffeetastingjournal-client/src/app/coffee.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Coffee } from './coffee';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoffeeService {
+
+  private apiServerUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) { }
+  
+  public getCoffees(): Observable<Coffee[]> {
+    return this.http.get<Coffee[]>(`${this.apiServerUrl}/coffee/all`);
+  }
+
+  public addCoffee(coffee: Coffee): Observable<Coffee> {
+    return this.http.post<Coffee>(`${this.apiServerUrl}/coffee/add`, coffee);
+  }
+
+  public updateCoffee(coffee: Coffee): Observable<Coffee> {
+    return this.http.put<Coffee>(`${this.apiServerUrl}/coffee/update`, coffee);
+  }
+
+  public deleteCoffee(coffeeId: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/coffee/delete/${coffeeId}`);
+  }
+}
