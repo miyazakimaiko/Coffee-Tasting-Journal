c1f4feafccadebc213e8aa637f9a76a9b42ed033
miyazakimaiko <myzkmik19922@gmail.com>
2021-07-14 23:25:46 +0100

    Added interface

---
+++ coffeetastingjournal-client/src/app/coffee.ts
@@ -0,0 +1,11 @@
+export interface Coffee {
+    id: number;
+    name: string;
+    roaster: string;
+    roastDate: string;
+    variety: string;
+    process: string;
+    brewMethod: string;
+    notes: string;
+    rating: number;
+}
