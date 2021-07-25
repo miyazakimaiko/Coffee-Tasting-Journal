423417bbc94e25cc6580e7b9105c97fb09772c78
miyazakimaiko <myzkmik19922@gmail.com>
2021-07-25 12:47:06 +0100

    Added @Transactional

--- coffeetastingjournal-server/src/main/java/com/miyazakimaiko/coffeetastingjournal/service/CoffeeService.java
+++ coffeetastingjournal-server/src/main/java/com/miyazakimaiko/coffeetastingjournal/service/CoffeeService.java
@@ -4,6 +4,7 @@ import com.miyazakimaiko.coffeetastingjournal.exception.CoffeeNotFoundException;
 import com.miyazakimaiko.coffeetastingjournal.model.Coffee;
 import com.miyazakimaiko.coffeetastingjournal.repo.CoffeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,22 +18,27 @@ public class CoffeeService {
         this.coffeeRepo = coffeeRepo;
     }
 
+    @Transactional
     public Coffee addCoffee(Coffee coffee) {
         return coffeeRepo.save(coffee);
     }
 
+    @Transactional
     public List<Coffee> findAllCoffees() {
         return coffeeRepo.findAll();
     }
 
+    @Transactional
     public Coffee updateCoffee(Coffee coffee) {
         return coffeeRepo.save(coffee);
     }
 
+    @Transactional
     public Coffee findCoffeeById(Long id) {
         return coffeeRepo.findCoffeeById(id).orElseThrow(() -> new CoffeeNotFoundException("Cannot find coffee with id " + id + "."));
     }
 
+    @Transactional
     public void deleteCoffee(Long id) {
         coffeeRepo.deleteCoffeeById(id);
     }
