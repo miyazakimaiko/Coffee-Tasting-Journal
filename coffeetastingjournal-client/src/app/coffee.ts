a1f5ad79f39ac15cd8b77380ec287bc8d5c6489e
miyazakimaiko <myzkmik19922@gmail.com>
2021-07-31 20:50:37 +0100

    Added more data for the entry

--- coffeetastingjournal-client/src/app/coffee.ts
+++ coffeetastingjournal-client/src/app/coffee.ts
@@ -1,11 +1,27 @@
-export interface Coffee {
-    id: number;
-    name: string;
-    roaster: string;
-    roastDate: string;
-    variety: string;
-    process: string;
-    brewMethod: string;
-    notes: string;
-    rating: number;
+export class Coffee {
+    id!: number;
+    name!: string;
+    roaster!: string;
+    roastDate!: string;
+    variety!: string;
+    process!: string;
+    brewMethod!: string;
+    notes!: string;
+    rating!: number;
+    sweet!: number;
+    acidic!: number;
+    floral!: number;
+    spicy!: number;
+    salty!: number;
+    berryfriut!: number;
+    citrusfruit!: number;
+    stonefruit!: number;
+    chocolate!: number;
+    caramel!: number;
+    smoky!: number;
+    bitter!: number;
+    savory!: number;
+    body!: number;
+    clean!: number;
+    linger!: number;
 }
